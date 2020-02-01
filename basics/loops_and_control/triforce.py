a0696205c0d2ddb7f79df86a17129d4860b91a3b
Macarthur Inbody <133794m3r@gmail.com>
2020-02-01 01:44:54 -0500

    updated it to work better.

--- basics/loops_and_control/triforce.py
+++ basics/loops_and_control/triforce.py
@@ -3,44 +3,52 @@
 string=''
 j=0;
 character=''
-numbers=10
-
-characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!-';
+numbers=20
+characters='#'*64
+#characters='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!-';
+print("Can I haz triforce?!")
 total_characters=0
 #draw top triangle.
 for i in range(numbers+1):
+	#select the chracter from the character list.
 	character=characters[i]
+	#make sure there's one extra space.
 	string=' '
+	#make numbers*2 -i be appened to string.
 	for j in range((numbers*2) -i ):
 		string+=' '
+	#then we add i*2 plus 1 characters be appeneded to the string.
 	for j in range(i*2+1):
 		string+=character
+	#print the string.
 	print(string)
 
 #then we draw the bottom two together.
 for i in range(numbers):
+	#make sure the string's empty.
 	string=''
+	#select the character.
 	character=characters[i]
-
-	string=''
-
+	#add numbers-i spaces to the string.
 	for j in range(numbers - i):
 		string+=' '
-
+	#then we add i*2 plus 1 characters to the string.
 	for j in range(i*2+1):
 		string+=character
-
+	#now for triangle 3 as it's inline with the second one. We add an additional space.
 	string+=' '
+	#then we add the number of lines -i times 2 spaces.
 	for j in range((numbers -i) *2 ):
 		string+=' '
-
-#	for j in range(numbers -i ):
-#		string+=' '
-	
+		
+	#then we add i*2+1 characters to the string.
 	for j in range(i*2+1):
 		string+=character
+	#quick hack to get total character counts.
 	total_characters+=len(string)
+	#print the generated string.
 	print(string)
+
 string=''
 character=characters[i+1]
 for j in range(i*2+3):
@@ -53,33 +61,7 @@ for j in range(i*2+3):
 
 total_characters+=len(string)
 print(string)
-'''
-while i>=0:
-	string=''
-	character=characters[i]
-	j=(numbers - i )
-	
-	while j > 0:
-		string+=' '
-		j-=1
-	
-	j=(i*2)
-	while j >=0:
-		string+=character
-		j-=1
-	
-	j=(numbers -i )*2-1
-	while j >= 0:
-		string+=' '
-		j-=1
 
-	j = (i*2)
-	while j >=0:
-		string+=character
-		j-=1
-	i-=1
-	total_characters+=len(string)
-	print(string)
-'''
-print("\nIn total to make the 2 triangles we used",total_characters,"to make 2 diamonds that were",(numbers*2)+1,"lines each.\nAlso we used",numbers+1,"unique characters.")
+
+print("\nIn total to make the triforce we used",total_characters,"to make 2 diamonds that were",(numbers*2)+1,"lines each.\nAlso we used",numbers+1,"unique characters.")
 print('{0};{2};{1}'.format(total_characters,(numbers*2)+1,numbers+2))

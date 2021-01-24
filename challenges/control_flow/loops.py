241163aa934d08890dc47b8a4dfc4f72cec9d9bb
Macarthur Inbody <133794m3r@gmail.com>
2021-01-24 12:15:53 -0500

    challenges/loops.py: A basic loop challenge for them to do in class. challenges/selections.py: A very basic filtering challenge.

---
+++ challenges/control_flow/loops.py
@@ -0,0 +1,19 @@
+"""
+Take all numbers from 1 to 110
+(inclusive meaning your counter should reach 110 before ending the loop)
+ and multiply it by itself plus 2 and then add that to our running total
+An example is below for the numbers 1  to 3(inclusive).
+
+0) total = 0.
+1) Start with the number 1.
+2) 1*(1+2) = 2.
+3) Add it to the total. total = total + 2.
+4) 2*(2+2) = 8.
+5) Add 8 to the total. total = total +8. Total is now 10.
+6) 3*(3+2) = 15.
+7) Add 15 to the total. total = total + 15. The total is now 25.
+8) Now you have your total and the answer would be 25.
+
+You can use a "for" or a "while" loop to get your answer.
+"""
+
---
+++ challenges/control_flow/selections.py
@@ -0,0 +1,25 @@
+"""Challenge is given below:
+
+You are given the list of numbers below. Go through all of the numbers in that list.
+If the number is divisible by 4 add it to the resulting list. If it is not do not add it.
+
+An example is right below here.
+If you were given the list of numbers [1,3,5,6]
+Go through them and create a list of numbers that are even.
+
+results = []
+1) Start with 1. It's less than 4 so we don't add it.
+2) Is 3 divisible by 3? yes because 1*3 = 3. So we add it to the resulting list.
+3) Is 5 divisible by 3? No so we skip it.
+4) Is 6 divisble by 3? Yes. Because 2*3 = 6. So we add it to the resulting list.
+5) The results list now looks like this when you print it out [3, 6].
+That is then your flag. You don't have to worry about whether or not your
+resulting list has a space between the comma all that matters is that the values are in it.
+
+The numbers below are very large but the rules for modulus still apply to them.
+"""
+
+your_numbers = [3758002251217574918, 11609088957906065718, 7717603874093113135, 5957909080447991310]
+results = []
+
+# TODO: Write your code to go through this list of numbers.
